6a36636a56dbae2e753d09be2cbd4a03be065116
PAX <mat.pxo@gmail.com>
2022-09-28 22:37:18 -0300

    avanços

---
+++ Scripts/index.js
@@ -0,0 +1,29 @@
+/*function slideNext(){
+    let slide = document.getElementsByClassName('slide');
+    let main = document.getElementsByClassName('slide main');
+    let next = document.getElementsByClassName('slide next');
+    let before = document.getElementsByClassName('slide before');
+    console.log(slide.length)
+    console.log(main.length)
+    console.log(typeof main)
+    
+   // main.classList.replace('slide main','silde next');
+    //document.getElementsById('slide').className.contains('main').replace('next');
+    //document.getElementsByClassName("slide main").className.replace("slide next")
+    //main.classList.add("next");
+
+    if(slide.classList.contains("slide main")){
+        slide.className = "slide before";
+        console.log(slide.length)
+    }
+    if(slide.className == "slide next"){
+        slide.className = "slide main";
+    }
+    if(slide.className == "slide before"){
+        slide.className = "slide next";
+    }
+
+    for ( i=0; i < slide.length; i++){
+
+    }
+}*/
